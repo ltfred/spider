14379aec2bf53bb933f943ee841194b9c66eb798
ltfred <ltfred@163.com>
2019-12-24 18:06:20 +0800

    修改模型类和增加评论回复数据抓取

--- models.py
+++ models.py
@@ -1,44 +1,64 @@
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("mysql+pymysql://logan:L123456@10.64.144.3:3306/vsblob")
 Base = declarative_base()
 
+
 class Video(Base):
+    # 视频信息表
     __tablename__ = 'douyin_video'
     id = Column(Integer, primary_key=True)
     aweme_id = Column(String(64))
     desc = Column(String(255))
     short_id = Column(String(32))
     bp_time = Column(String(16))
-    music = Column(String(255))
-    video = Column(Text)
+    music_url = Column(String(255))
+    video_url = Column(Text)
     comment_count = Column(Integer)
     digg_count = Column(Integer)
     share_count = Column(Integer)
 
 
 class User(Base):
+    # 用户信息表
     __tablename__ = 'douyin_user_info'
     id = Column(Integer, primary_key=True)
     short_id = Column(String(32))
     city = Column(String(16))
+    country = Column(String(8))
+    province = Column(String(8))
     nickname = Column(String(16))
     follower_count = Column(Integer)
+    following_count = Column(Integer)
+    aweme_count = Column(Integer)
+    favoriting_count = Column(Integer)
+    dongtai_count = Column(Integer)
+    total_favorited_count = Column(Integer)
+    birthday = Column(String(32))
+    signature = Column(Text)
 
 
-class Follow(Base):
-    __tablename__ = 'douyin_follow'
+class Follower(Base):
+    # 粉丝信息表
+    __tablename__ = 'douyin_follower'
     id = Column(Integer, primary_key=True)
-    following = Column(String(16))
-    follower = Column(String(16))
+    following = Column(String(32))
+    follower = Column(String(32))
 
+
+class Following(Base):
+    # 关注信息表
+    __tablename__ = 'douyin_following'
+    id = Column(Integer, primary_key=True)
+    following = Column(String(32))
+    follower = Column(String(32))
 
 
 class Comment(Base):
+    # 视频评论表
     __tablename__ = 'douyin_comment'
-    uid = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     aweme_id = Column(String(64))
     author = Column(String(32))
     content = Column(Text)
--- spider.py
+++ spider.py
@@ -12,44 +12,50 @@ class Spider(object):
     def response(self, flow: http.HTTPFlow):
         video_url = 'https://aweme-ipv6.snssdk.com/aweme/v1/aweme/post/'
         user_info_url = 'https://aweme-eagle-ipv6.snssdk.com/aweme/v1/user/'
-        following_url = 'https://aweme-ipv6.snssdk.com/aweme/v1/user/follower/list/'
+        follower_url = 'https://aweme-ipv6.snssdk.com/aweme/v1/user/follower/list/'
+        following_url = 'https://aweme-ipv6.snssdk.com/aweme/v1/user/following/list/'
         comment_url = 'https://aweme-ipv6.snssdk.com/aweme/v2/comment/list/'
+        comment_reply_url = 'https://aweme-ipv6.snssdk.com/aweme/v1/comment/list/reply/'
         if flow.request.url.startswith(video_url):
-            print('视频-----------')
+            print('视频信息-----------')
             response = flow.response.get_text()
             self.parse_video_response(response)
         elif flow.request.url.startswith(user_info_url):
             print('用户信息------------')
             response = flow.response.get_text()
             self.parse_user_info_response(response)
-        elif flow.request.url.startswith(following_url):
+        elif flow.request.url.startswith(follower_url):
             print('用户粉丝------------')
             response = flow.response.get_text()
-            self.parse_user_follow_response(response)
+            self.parse_user_follower_response(response)
         elif flow.request.url.startswith(comment_url):
             print('视频评论------------')
             response = flow.response.get_text()
             self.parse_comment_response(response)
+        elif flow.request.url.startswith(following_url):
+            print('用户关注-------------')
+            response = flow.response.get_text()
+            self.parse_user_following_response(response)
+        elif flow.request.url.startswith(comment_reply_url):
+            print('评论回复-------------')
+            response = flow.response.get_text()
+            self.parse_comment_reply_response(response)
 
     def parse_video_response(self, response):
         print('running ....... parse video response')
         json_dict = json.loads(response)
         aweme_list = json_dict['aweme_list']
         for aweme in aweme_list:
+            obj = Video(aweme_id=aweme['aweme_id'], desc=aweme['desc'], bp_time=aweme['create_time'],
+                        music_url=str(aweme['music']['play_url']['uri']),
+                        video_url=aweme['video']['play_addr']['url_list'][0],
+                        comment_count=int(aweme['statistics']['comment_count']),
+                        digg_count=int(aweme['statistics']['digg_count']),
+                        share_count=int(aweme['statistics']['share_count']))
             if aweme['author']['unique_id'] != '':
-                obj = Video(aweme_id=aweme['aweme_id'], desc=aweme['desc'], bp_time=aweme['create_time'],
-                            short_id=str(aweme['author']['unique_id']), music=str(aweme['music']['play_url']['uri']),
-                            video=aweme['video']['play_addr']['url_list'][0],
-                            comment_count=int(aweme['statistics']['comment_count']),
-                            digg_count=int(aweme['statistics']['digg_count']),
-                            share_count=int(aweme['statistics']['share_count']))
+                obj.short_id = str(aweme['author']['unique_id'])
             else:
-                obj = Video(aweme_id=aweme['aweme_id'], desc=aweme['desc'], bp_time=aweme['create_time'],
-                            short_id=str(aweme['author']['short_id']), music=str(aweme['music']['play_url']['uri']),
-                            video=aweme['video']['play_addr']['url_list'][0],
-                            comment_count=int(aweme['statistics']['comment_count']),
-                            digg_count=int(aweme['statistics']['digg_count']),
-                            share_count=int(aweme['statistics']['share_count']))
+                obj.short_id = str(aweme['author']['short_id'])
             session.add(obj)
             session.commit()
 
@@ -57,33 +63,49 @@ class Spider(object):
         print('running ....... parse user info response')
         with open('user_data.txt', 'w') as w:
             w.write(response)
-        dict = json.loads(response)
-        if dict['user']['short_id'] == '0':
-            obj = User(short_id=dict['user']['unique_id'], city=dict['user']['city'],
-                       nickname=dict['user']['nickname'], follower_count=dict['user']['follower_count'])
+        user_dict = json.loads(response)
+        obj = User(city=user_dict['user']['city'], country=user_dict['user']['country'],
+                   province=user_dict['user']['province'], nickname=user_dict['user']['nickname'],
+                   follower_count=user_dict['user']['follower_count'], following_count=user_dict['user']['following_count'],
+                   aweme_count=user_dict['user']['aweme_count'], favoriting_count=user_dict['user']['favoriting_count'],
+                   dongtai_count=user_dict['user']['dongtai_count'],
+                   total_favorited_count=user_dict['user']['total_favorited'], birthday=user_dict['user']['birthday'],
+                   signature=user_dict['user']['signature'])
+        if user_dict['user']['short_id'] == '0':
+            obj.short_id = user_dict['user']['unique_id']
         else:
-            obj = User(short_id=dict['user']['short_id'], city=dict['user']['city'],
-                       nickname=dict['user']['nickname'], follower_count=dict['user']['follower_count'])
+            obj.short_id = user_dict['user']['short_id']
         session.add(obj)
         session.commit()
 
-    def parse_user_follow_response(self, response):
+    def parse_user_follower_response(self, response):
         print('running ....... parse user follow response')
-        with open('user_follow_data.txt', 'w') as w:
+        with open('user_follower_data.txt', 'w') as w:
             w.write(response)
 
     def parse_comment_response(self, response):
         print('running parse comment function .......')
-        dict = json.loads(response)
-        comments = dict['comments']
-        for comment in comments:
-            if comment['user']['short_id'] != '0':
-                obj = Comment(aweme_id=comment['aweme_id'], content=comment['text'], time=str(comment['create_time']), author=comment['user']['short_id'])
-            else:
-                obj = Comment(aweme_id=comment['aweme_id'], content=comment['text'], time=str(comment['create_time']),
-                              author=comment['user']['unique_id'])
-            session.add(obj)
-            session.commit()
+        comment_dict = json.loads(response)
+        comments = comment_dict['comments']
+        if len(comments) != 0:
+            for comment in comments:
+                obj = Comment(aweme_id=comment['aweme_id'], content=comment['text'], time=str(comment['create_time']))
+                if comment['user']['short_id'] != '0':
+                    obj.author = comment['user']['short_id']
+                else:
+                    obj.author = author=comment['user']['unique_id']
+                session.add(obj)
+                session.commit()
+
+    def parse_user_following_response(self, response):
+        print('running ....... parse user following response')
+        with open('user_following_data.txt', 'w') as w:
+            w.write(response)
+
+    def parse_comment_reply_response(self, response):
+        print('running ....... parse user following response')
+        with open('comment_reply_data.txt', 'w') as w:
+            w.write(response)
 
 
 addons = [
